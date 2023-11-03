7b73c62e33509fd50da774eca482b356c696e995
R-Strange <richardpstrange@outlook.com>
2023-11-03 15:47:02 +0000

    increased code coverage, better print logic, times in datetime

--- my_weather/src/my_weather.py
+++ my_weather/src/my_weather.py
@@ -4,6 +4,7 @@ import argparse
 import json
 from urllib import parse, request
 import os
+from datetime import datetime
 CREDENTIALS_PATH: str = "./creds.yaml"
 abs_credentials_path: str = os.path.abspath(CREDENTIALS_PATH)
 
@@ -41,18 +42,24 @@ def parse_user_cli_args() -> argparse.Namespace:
     
     return parser.parse_args()
 
-def api_url_query_string_builder(city: List[str], imperial: bool = False) -> str:
+def api_url_query_string_builder(open_weather_api_key: str, city: List[str], imperial: bool = False) -> str:
     """
     Builds the URL query string for the OpenWeather API.
-    example API string: https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=1234567890abcdef1234567890abcdef
+    example API string: https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=1234567890abcdef1234567890abcdef  
+    Where no API key is provided, an API key and field is not included in the query string.
     """
     root_url: str = "https://api.openweathermap.org/data/2.5/weather"
     if imperial:
         units: str = "imperial"
     else:
         units: str = "metric"
+    if not isinstance(city, list):
+        raise TypeError("city must be a list of strings")
     city_string: str = " ".join(city)  # joins  multiple word cities (e.g. New York) into a single string.
-    query_string: str = f"{root_url}?q={city_string}&units={units}&appid={open_weather_api_key}"
+    if open_weather_api_key is None:
+        query_string: str = f"{root_url}?q={city_string}&units={units}"
+    else:
+        query_string: str = f"{root_url}?q={city_string}&units={units}&appid={open_weather_api_key}"
 
     return query_string
 
@@ -64,21 +71,31 @@ def get_weather_data(query_url: str) -> Dict:
     
 def print_weather_data(weather_data: Dict) -> None:
     """Prints the weather data to the console."""
-    print(f"Current weather in {weather_data['name']}:")
+    if 'sys' in weather_data and 'sunrise' in weather_data['sys']:
+        sunrise_time: datetime = datetime.fromtimestamp(weather_data['sys']['sunrise']).strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        sunrise_time = 'N/A'
+    
+    if 'sys' in weather_data and 'sunset' in weather_data['sys']:
+        sunset_time: datetime = datetime.fromtimestamp(weather_data['sys']['sunset']).strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        sunset_time = 'N/A'
+    
+    print(f"Current weather in {weather_data.get('name', 'N/A')}:")
     print(f"  Conditions: {weather_data['weather'][0]['description']}")
-    print(f"  Temperature: {weather_data['main']['temp']} degrees")
-    print(f"  Feels like: {weather_data['main']['feels_like']} degrees")
-    print(f"  Humidity: {weather_data['main']['humidity']}%")
-    print(f"  Wind: {weather_data['wind']['speed']} mph")
-    print(f"  Wind direction: {weather_data['wind']['deg']} degrees")
-    print(f"  Cloud cover: {weather_data['clouds']['all']}%")
-    print(f"  Sunrise: {weather_data['sys']['sunrise']}")
-    print(f"  Sunset: {weather_data['sys']['sunset']}")
+    print(f"  Temperature: {weather_data['main'].get('temp', 'N/A')} degrees")
+    print(f"  Feels like: {weather_data['main'].get('feels_like', 'N/A')} degrees")
+    print(f"  Humidity: {weather_data['main'].get('humidity', 'N/A')}%")
+    print(f"  Wind: {weather_data['wind'].get('speed', 'N/A')} mph")
+    print(f"  Wind direction: {weather_data['wind'].get('deg', 'N/A')} degrees")
+    print(f"  Cloud cover: {weather_data['clouds'].get('all', 'N/A')}%")
+    print(f"  Sunrise: {sunrise_time}")
+    print(f"  Sunset: {sunset_time}")
 
 if __name__ == "__main__":
     cli_args: argparse.Namespace = parse_user_cli_args()
     open_weather_api_key: str = read_creds()
-    query_url: str = api_url_query_string_builder(cli_args.city, cli_args.imperial)
+    query_url: str = api_url_query_string_builder(open_weather_api_key ,cli_args.city, cli_args.imperial)
     print_weather_data(get_weather_data(query_url))
     
     
--- tests/test_myweather.py
+++ tests/test_myweather.py
@@ -1,9 +1,9 @@
-from my_weather.src.my_weather import read_creds
+from my_weather.src.my_weather import read_creds, parse_user_cli_args, api_url_query_string_builder, print_weather_data
 import yaml
+import argparse
 
 
-# Dependencies:
-# pip install pytest-mock
+# needs pytest-mock!!!
 import pytest
 
 class TestReadCreds:
@@ -76,3 +76,396 @@ class TestReadCreds:
         # Call the read_creds function and assert that it raises a yaml.YAMLError
         with pytest.raises(yaml.YAMLError):
             read_creds()
+            
+class TestParseUserCliArgs:
+
+    # The function correctly parses a city name as a positional argument.
+    def test_parse_city_name_positional_argument(self, mocker):
+        # Given
+        city_name = "New York"
+        mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(city=city_name))
+    
+        # When
+        result = parse_user_cli_args()
+    
+        # Then
+        assert result.city == city_name
+
+    # The function correctly parses the imperial flag as an optional argument.
+    def test_parse_imperial_flag_optional_argument(self, mocker):
+        # Given
+        mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(imperial=True))
+    
+        # When
+        result = parse_user_cli_args()
+    
+        # Then
+        assert result.imperial is True
+
+    # The function returns an argparse.Namespace object with the parsed arguments.
+    def test_return_argparse_namespace_object(self, mocker):
+        # Given
+        city_name = "New York"
+        mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(city=city_name, imperial=True))
+    
+        # When
+        result = parse_user_cli_args()
+    
+        # Then
+        assert isinstance(result, argparse.Namespace)
+        assert result.city == city_name
+        assert result.imperial is True
+
+    # The function should return the open weather API key when no city name is provided.
+    def test_no_city_name_return_value(self, mocker):
+        # Given
+        mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(city=None))
+
+        # When
+        result = parse_user_cli_args()
+
+        # Then
+        assert result == argparse.Namespace(city=None)
+
+    # The function should raise a SystemExit when an invalid flag is provided.
+    def test_raise_error_invalid_flag_fixed(self, mocker):
+        # Given
+        mocker.patch("argparse.ArgumentParser.parse_args", side_effect=SystemExit)
+
+        # When/Then
+        with pytest.raises(SystemExit):
+            parse_user_cli_args()
+
+    # The function does not raise a ValueError when an invalid city name is provided.
+    def test_raise_error_invalid_city_name(self, mocker):
+        # Given
+        mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(city="1234"))
+
+        # When/Then
+        parse_user_cli_args()
+        
+class TestApiUrlQueryStringBuilder:
+
+    # Builds the URL query string with metric units by default.
+    def test_builds_url_query_string_with_metric_units_by_default(self):
+        # Given
+        city = ["London"]
+        imperial = False
+        open_weather_api_key = "1234567890abcdef1234567890abcdef"
+
+        # When
+        result = api_url_query_string_builder(open_weather_api_key, city, imperial)
+
+        # Then
+        assert result == "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=1234567890abcdef1234567890abcdef"
+
+    # Builds the URL query string with imperial units when imperial=True.
+    def test_builds_url_query_string_with_imperial_units_when_imperial_true(self):
+        # Given
+        city = ["London"]
+        imperial = True
+        open_weather_api_key = "1234567890abcdef1234567890abcdef"
+
+        # When
+        result = api_url_query_string_builder(open_weather_api_key, city, imperial)
+
+        # Then
+        assert result == "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=1234567890abcdef1234567890abcdef"
+
+    # Joins multiple word cities into a single string for the query string.
+    def test_joins_multiple_word_cities_into_single_string_for_query_string(self):
+        # Given
+        city = ["New", "York"]
+        imperial = False
+        open_weather_api_key = "1234567890abcdef1234567890abcdef"
+
+        # When
+        result = api_url_query_string_builder(open_weather_api_key, city, imperial)
+
+        # Then
+        assert result == "https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&appid=1234567890abcdef1234567890abcdef"
+
+    # Ensures that the query string does not contain 'appid=' if the open_weather_api_key variable is None.
+    def test_does_not_contain_appid_if_open_weather_api_key_variable_is_none(self):
+        # Given
+        city = ["London"]
+        imperial = False
+        open_weather_api_key = None
+
+        # When
+        query_string = api_url_query_string_builder(open_weather_api_key, city, imperial)
+
+        # Then
+        assert 'appid=' not in query_string
+
+    # Raises a TypeError if the city argument is not a list.
+    def test_raises_type_error_if_city_argument_not_list(self):
+        # Given
+        city = "London"
+        imperial = False
+        open_weather_api_key = "1234567890abcdef1234567890abcdef"
+
+        # When, Then
+        with pytest.raises(TypeError):
+            query_string = api_url_query_string_builder(open_weather_api_key, city, imperial)
+
+    # The test should not expect a ValueError to be raised when the city argument is an empty list.
+    def test_does_not_raise_value_error_if_city_argument_empty_list(self):
+        # Given
+        city = []
+        imperial = False
+        open_weather_api_key = "1234567890abcdef1234567890abcdef"
+
+        # When, Then
+        api_url_query_string_builder(open_weather_api_key, city, imperial)
+
+class TestPrintWeatherData:
+
+    # Should print the weather data to the console
+    def test_print_weather_data_console(self):
+        import io
+        import sys
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunrise': 1634567890, 'sunset': 1634607890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: 2021-10-18 15:38:10\n" \
+                          "  Sunset: 2021-10-19 02:44:50\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Should print the weather data for a given city
+    def test_print_weather_data_city_name(self):
+        import io
+        import sys  # Import the sys module
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunrise': 1634567890, 'sunset': 1634607890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: 2021-10-18 15:38:10\n" \
+                          "  Sunset: 2021-10-19 02:44:50\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Should print the weather conditions
+    def test_print_weather_data_conditions(self):
+        import io
+        import sys
+
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunrise': 1634567890, 'sunset': 1634607890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: 2021-10-18 15:38:10\n" \
+                          "  Sunset: 2021-10-19 02:44:50\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Should handle missing 'sys' key in weather_data
+    def test_print_weather_data_missing_sys_key_fixed(self):
+        import io
+        import sys
+
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: N/A\n" \
+                          "  Sunset: N/A\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Should handle missing 'sunrise' key in weather_data['sys']
+    def test_print_weather_data_missing_sunrise_key_fixed(self):
+        import io
+        import sys
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunset': 1634607890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: N/A\n" \
+                          "  Sunset: 2021-10-19 02:44:50\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Should handle missing 'sunset' key in weather_data['sys']
+    def test_print_weather_data_missing_sunset_key_fixed(self):
+        import io
+        import sys
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunrise': 1634567890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: 2021-10-18 15:38:10\n" \
+                          "  Sunset: N/A\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Test that the printed sunrise and sunset times are in the correct format
+    def test_printed_times_format(self):
+        import io
+        import sys
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunrise': 1634567890, 'sunset': 1634607890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: 2021-10-18 15:38:10\n" \
+                          "  Sunset: 2021-10-19 02:44:50\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
+
+    # Test that the printed sunrise and sunset times are in the correct format
+    def test_printed_times_format(self):
+        import io
+        import sys
+        weather_data = {
+            'name': 'City',
+            'weather': [{'description': 'Cloudy'}],
+            'main': {'temp': 20, 'feels_like': 18, 'humidity': 80},
+            'wind': {'speed': 10, 'deg': 180},
+            'clouds': {'all': 50},
+            'sys': {'sunrise': 1634567890, 'sunset': 1634607890}
+        }
+        expected_output = "Current weather in City:\n" \
+                          "  Conditions: Cloudy\n" \
+                          "  Temperature: 20 degrees\n" \
+                          "  Feels like: 18 degrees\n" \
+                          "  Humidity: 80%\n" \
+                          "  Wind: 10 mph\n" \
+                          "  Wind direction: 180 degrees\n" \
+                          "  Cloud cover: 50%\n" \
+                          "  Sunrise: 2021-10-18 15:38:10\n" \
+                          "  Sunset: 2021-10-19 02:44:50\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_weather_data(weather_data)
+
+        sys.stdout = sys.__stdout__
+        assert captured_output.getvalue() == expected_output
